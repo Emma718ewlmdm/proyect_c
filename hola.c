05625680b40f043b2a563703065991ab5777e5cb
Emma718ewlmdm <emmaanuelgonf@gmail.com>
2026-03-19 22:16:26 +0000

    chose

--- hola.c
+++ hola.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo{
+    int data;
+    struct nodo* next;
+}Nodo;
+
+typedef Nodo* Link;
+
+Link push(Link head, int val){
+    Link new = (Link)malloc(sizeof(Nodo));
+    new->data = val;
+    new->next = head;
+    return new;
+}
+
+void print_ls(Link head){
+    while(head != NULL){
+        printf("[%d] -> ",head->data);
+        head = head->next;
+    }
+    printf("NULL\n\n");
+}
 
 int main(){
 
-    printf("Hola mundo\n\n");
+    Link lista1 = NULL;
+
+    lista1 = push(lista1,0);
+    lista1 = push(lista1,1);
+    lista1 = push(lista1,2);
+
+    print_ls(lista1);
 
     return 0;
 }
